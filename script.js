d1b7a0a22546be46a2db41298e55cbf645e1cc04
roland6321 <160388092+Roland6321@users.noreply.github.com>
2024-02-18 10:27:56 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function openOptionsModal(itemName) {
+    document.getElementById('menuItemName').innerText = itemName;
+    document.getElementById('optionsModal').style.display = 'block';
+}
+
+function closeOptionsModal() {
+    document.getElementById('optionsModal').style.display = 'none';
+}
+
+function submitOptions() {
+    var itemName = document.getElementById('menuItemName').innerText;
+    var extras = document.getElementById('extraOptions').value;
+    var comment = document.getElementById('comment').value;
+    alert('You ordered ' + itemName + ' with ' + extras + '. Comment: ' + comment);
+    closeOptionsModal();
+}
