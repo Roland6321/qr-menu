188a32d66f583fd4de059b7c870989c4aa9d5ac1
roland6321 <160388092+Roland6321@users.noreply.github.com>
2024-02-19 13:01:44 +0000

    Troubleshooting so it works on mobile

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function submitOptions() {
     closeOptionsModal();
 }
 
-document.getElementById('startButton').addEventListener('click', function() {
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', function() {
     document.getElementById('welcomeScreen').style.display = 'none';
     document.getElementById('menuSection').style.display = 'block';
 });
+startButton.addEventListener('touchstart', function() {
+    document.getElementById('welcomeScreen').style.display = 'none';
+    document.getElementById('menuSection').style.display = 'block';
+}, false);
+
