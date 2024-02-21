60273db0cc875e11692c05a4f41131cb9e9a73f2
roland6321 <160388092+Roland6321@users.noreply.github.com>
2024-02-21 10:40:40 +0000

    revised version

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('startButton');
     const startMenu = document.getElementById('startMenu');
-    const userPrompt = document.getElementById('userPrompt');
+    const categories = document.getElementById('categories');
 
     startButton.addEventListener('click', () => {
         // Hide the start menu and show the user prompt
         startMenu.style.display = 'none';
-        userPrompt.style.display = 'block';
+        categories.style.display = 'block';
     });
 });
