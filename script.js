8f51cc4e4934238060116c0d195dbff349267892
roland6321 <160388092+Roland6321@users.noreply.github.com>
2024-02-25 09:58:01 +0000

    Restoring previous version

--- script.js
+++ script.js
@@ -5,44 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuItemsSections = document.querySelectorAll('.menu-items');
     const backToCategoriesButtons = document.querySelectorAll('.backToCategories');
 
+    // Function to show the categories page
     function showCategories() {
         categories.style.display = 'block';
         menuItemsSections.forEach(item => item.style.display = 'none');
         document.getElementById('startMenu').style.display = 'none';
     }
 
+    // Function to show menu items for a specific category
     function showMenuItems(index) {
         categories.style.display = 'none';
         document.getElementById('startMenu').style.display = 'none';
         menuItemsSections[index].style.display = 'block';
     }
 
-    function showMenuItemDetails(itemId) {
-        menuItemsSections.forEach(section => section.style.display = 'none');
-        document.getElementById(`${itemId}DetailsPage`).style.display = 'block';
-    }
-
-    startButton.addEventListener('click', showCategories);
+    startButton.addEventListener('click', () => {
+        showCategories();
+    });
 
     categoryButtons.forEach((button, index) => {
-        button.addEventListener('click', () => showMenuItems(index));
-    });
-
-    backToCategoriesButtons.forEach(button => {
-        button.addEventListener('click', showCategories);
-    });
-
-    document.querySelectorAll('.item-cta').forEach(button => {
         button.addEventListener('click', () => {
-            const itemId = button.closest('.menu-item').getAttribute('data-item-id');
-            showMenuItemDetails(itemId);
+            showMenuItems(index);
         });
     });
 
-    document.querySelectorAll('.backToMenu').forEach(button => {
+    backToCategoriesButtons.forEach(button => {
         button.addEventListener('click', () => {
-            document.querySelectorAll('.menu-item-details').forEach(detailPage => detailPage.style.display = 'none');
-            showCategories(); // or showMenuItems(index) if you want to go back to the specific category
+            showCategories();
         });
     });
 });
