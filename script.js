2ef159c6f0caa363941a28662e890d0659ddcbe9
roland6321 <radocz.roland@gmail.com>
2024-03-31 09:46:56 +0200

    fixing

--- script.js
+++ script.js
@@ -183,14 +183,14 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('cartSection').style.display = 'block';
     });
 
-    // Ensures the counter is immediately updated on page load.
-    calculateAndDisplayTotalCost(JSON.parse(localStorage.getItem('cart')) || []);
-
     // Add the new functionality here, just before the closing of the DOMContentLoaded listener
     document.querySelector('.proceed-to-payment-btn').addEventListener('click', function() {
         document.getElementById('cartSection').style.display = 'none';
-        document.getElementById('paymentSection').style.display = 'block';
+         document.getElementById('paymentSection').style.display = 'block';
     });
+
+    // Ensures the counter is immediately updated on page load.
+    calculateAndDisplayTotalCost(JSON.parse(localStorage.getItem('cart')) || []);
 });
 
 
